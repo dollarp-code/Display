8a5fcde6af960fb35b6398eee052f0fb4ecde922
dollarp-code <husseindollarp@yahoo.com>
2024-03-13 17:14:07 +0100

    Updated the logic

--- script.js
+++ script.js
@@ -4,23 +4,31 @@
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
 const message = document.querySelector(".msg");
 const images = document.querySelector(".images");
 
 btn1.addEventListener("click", function () {
   message.innerHTML = "Button One is clicked";
   message.style.backgroundColor = "orange";
+  images.classList.remove("hidden");
   images.src = "assets/image-1.avif";
 });
 btn2.addEventListener("click", function () {
   message.innerHTML = "Button Two is clicked";
   message.style.backgroundColor = "red";
+  images.classList.remove("hidden");
   images.src = "assets/image-2.avif";
 });
 btn3.addEventListener("click", function () {
   message.innerHTML = "Button Three is clicked";
   message.style.backgroundColor = "green";
+  images.classList.remove("hidden");
   images.src = "assets/image-3.jpeg";
+});
+btn4.addEventListener("click", function () {
+  message.innerHTML = "";
+  images.classList.add("hidden");
 });
 
 let quaid = ["1"];
