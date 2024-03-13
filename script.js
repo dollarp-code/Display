1e2ee18d2d5937ab9503b46a69d84364604e77f5
dollarp-code <husseindollarp@yahoo.com>
2024-03-13 17:00:24 +0100

    quiz

--- script.js
+++ script.js
@@ -34,3 +34,26 @@ console.log(quaid.length, quaid, team, tech);
 let x = true === [];
 let y = true === ![];
 console.log(x + y, x, y);
+
+function countPositivesSumNegatives(input) {
+  if (input === null || input.length === 0) {
+    return [];
+  }
+  let posCount = 0;
+  let negSum = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      posCount++;
+    } else {
+      negSum += input[i];
+    }
+  }
+  return [posCount, negSum];
+}
+console.log(countPositivesSumNegatives([]));
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -5, -3, -9, -8, -12, -4, -17, -20, -17, 9,
+    17,
+  ])
+);
